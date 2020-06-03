376fe3f717f7baf568455b2da3e9aad761a95f0c
Ubuntu <ubuntu@ip-172-31-5-93.ap-northeast-1.compute.internal>
2020-06-03 09:21:18 +0000

    try react

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function home(){
-  return{
+function Home(){
+  return(
     <div>
-    
+      <h1>HOME</h1>
+      home is .....
     </div>
-  }
-}
+  );
+};
 
+function Topics(){
+  return(
+    <div>
+      <h1>Topics</h1>
+      topic is .....
+    </div>
+  );
+};
+
+function Content(){
+  return(
+    <div>
+      <h1>Content</h1>
+      Content is .....
+    </div>
+  );
+};
 
 
 
@@ -18,7 +36,9 @@ function App() {
   return (
     <div>
       <h1>React Router DOM example</h1>
-      
+      <Route><Home></Home></Route>
+      <Route><Topics></Topics></Route>
+      <Route><Content></Content></Route>
     </div>
   );
 }
