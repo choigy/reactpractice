86d2f30d96c192437f624a9c7f9fa460a78ef071
Ubuntu <ubuntu@ip-172-31-5-93.ap-northeast-1.compute.internal>
2020-06-03 03:51:36 +0000

    first store

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function home(){
+  return{
+    <div>
+    
+    </div>
+  }
+}
+
+
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Router DOM example</h1>
+      
     </div>
   );
 }
